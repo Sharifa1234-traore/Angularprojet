920147409e523a49a4f6fbdef65c51ef98d631fb
Sharifa1234-traore <traoresharifa71@gmail.com>
2025-03-04 14:48:53 +0000

    dernière modification du projet

--- src/app/page/page.component.ts
+++ src/app/page/page.component.ts
@@ -4,23 +4,22 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-page',
   templateUrl: './page.component.html',
-  styleUrls: ['./page.component.css']
+  styleUrls: ['./page.component.css'],
 })
-export class PageComponent implements OnInit{
+export class PageComponent implements OnInit {
   //  title = 'reproductiondepage';
   // // titre: string = 'Bienvenue sur myKN';
 
   // titre= {titre:"Bienvenue sur myKN",
   // nom:"Contrôle complet de vos envois",para:"Trouvez des devis de fret compétitifs, effectuez des réservations instantanées et suivez vos envois avec myKN.",}
 
-
-  constructor (private router: Router) {}
+  constructor(private router: Router) {}
   ngOnInit(): void {}
 
-onInscrire(){
-  this.router.navigate(['/inscription']);
-}
-onConnecter(){
-  this.router.navigate(['/connexion']);
-}
+  onInscrire() {
+    this.router.navigate(['/inscription']);
+  }
+  onConnecter() {
+    this.router.navigate(['/connexion']);
+  }
 }
